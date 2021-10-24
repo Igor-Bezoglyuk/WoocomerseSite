80204234c7e6a03c618e9dfc97edc46db691f36c
Igor-Bezoglyuk <78695421+Igor-Bezoglyuk@users.noreply.github.com>
2021-10-25 02:52:48 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+jQuery( document ).ready(function(){
+    $(function (){
+    $(".header-open-search").click(function(){
+    $(".header-form-search:hidden").show();
+    $(".header-open-search").hide();
+ });
+});
+});
